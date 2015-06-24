006ad636c805b8f72fd059641682737df2a5078d
pedroramus <p.i.c.m.ramos@gmail.com>
2015-06-24 02:45:53 -0300

    chatAPP creating roomlist with socket.io
    Making connection between the page the and the server thorught the jquery.
    You can create a room in the page

--- chatApp/socket/socket.js
+++ chatApp/socket/socket.js
@@ -1,5 +1,13 @@
-module.exports = function(io){
+module.exports = function(io, rooms){
 	var chatrooms = io.of('/roomlist').on('connection', function(socket){
 		console.log("connection is established on the server!");
+		socket.emit('roomupdate', JSON.stringify(rooms));
+
+		socket.on('newroom', function(data){
+			rooms.push(data);
+			socket.broadcast.emit('roomupdate', JSON.stringify(rooms));
+			socket.emit('roomupdate', JSON.stringify(rooms));
+		});
+
 	});
 }
