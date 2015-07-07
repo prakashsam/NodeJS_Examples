1a65dafb37af6aec536ac32674dba8bebdb03d6d
pedroramus <p.i.c.m.ramos@gmail.com>
2015-07-07 00:48:09 -0300

    Updating actives users in a chatroom

--- chatApp/socket/socket.js
+++ chatApp/socket/socket.js
@@ -11,16 +11,48 @@ module.exports = function(io, rooms){
 
 	});
 
+	function getUsersFromRoom(room){
+		var usersArray = [];
+		var nsp = io.of('/messages');
+		var clientsInRoom = nsp.adapter.rooms[room];
+		
+		for(var client in clientsInRoom){
+			usersArray.push(nsp.connected[client]);
+		}
+		
+		return usersArray;
+	}
+
+
 	var messages = io.of('/messages').on('connection', function(socket){
 		console.log('Connected to the chatroom!');
 		socket.on('joinroom', function(data){
 			socket.username = data.user;
 			socket.userPic = data.userPic;
 			socket.join(data.room);
+			updateUserList(data.room, true);
 		})
 
 		socket.on('newMessage', function(data){
 			socket.broadcast.to(data.room_number).emit('messagefeed', JSON.stringify(data));
-		})
+		});
+
+		function updateUserList(room, updateAll){
+			var getUsers = getUsersFromRoom(room);
+			var userlist = [];
+			for(var i in getUsers){
+				userlist.push({user:getUsers[i].username, userPic:getUsers[i].userPic});
+			}
+
+			socket.to(room).emit('updateUsersList', JSON.stringify(userlist));
+
+			if(updateAll){
+				socket.broadcast.to(room).emit('updateUsersList', JSON.stringify(userlist));
+			}
+		}
+
+		socket.on('updateList', function(data){
+			updateUserList(data.room);
+		});
 	});
 }
